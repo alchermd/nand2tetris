f26b538cb30e0b66c4662069c0b883de2b704dcb
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-01-05 15:01:53 +0800

    Add labels for key press/release handling. Currently writes to R0 for testing

--- chapters/04/fill/Fill.asm
+++ chapters/04/fill/Fill.asm
@@ -8,7 +8,33 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
+(ONPRESS)
+    // Writes 1 to R0 when a key is pressed.
+    // TODO: Write to the screen instead.
+    @R0
+    M=1
+    @POSTKEYPRESSCHECK
+    0;JMP
+
+(ONRELEASE)
+    // Writes 0 to R0 when a key is pressed.
+    // TODO: Write to the screen instead.
+    @R0
+    M=0
+    @POSTKEYPRESSCHECK
+    0;JMP
+
 (LOOP)
+    // Listen to keypress.
+    @KBD
+    D=M
+    @ONPRESS
+    D;JNE
+    @ONRELEASE
+    D; JEQ
+
+    (POSTKEYPRESSCHECK)
+
     // Infinite loop.
     @LOOP
-    0; JMP
+    0;JMP
